357b53b8f806adb1fb50732f402e06a7290c9299
diomededavid <david@d2creativedesign.com>
2023-11-04 12:31:00 -0700

    adds custom transformations and includes scss variables

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const { registerTransforms } = require('@tokens-studio/sd-transforms');
 const StyleDictionary = require('style-dictionary-utils');
 
 registerTransforms(StyleDictionary);
+require('./custom-transforms.js');
 
 const sd = StyleDictionary.extend({
   source: ['**/tokens.json'],
@@ -22,7 +23,13 @@ const sd = StyleDictionary.extend({
         'ts/color/css/hexrgba',
         'ts/color/modifiers',
         'name/cti/kebab',
+        'size/px-to-rem',
+        'clamp/css',
+        'dimension/pixelToRem'
       ],
+      options: {
+        basePxFontSize: 16
+      },
       buildPath: 'build/css/',
       files: [
         {
@@ -31,7 +38,42 @@ const sd = StyleDictionary.extend({
         },
       ],
     },
+    scss: {
+      transforms: [
+        'fontWeight/number',
+        'ts/descriptionToComment',
+        'ts/opacity',
+        'ts/size/lineheight',
+        'ts/typography/fontWeight',
+        'ts/resolveMath',
+        'ts/size/css/letterspacing',
+        'ts/typography/css/fontFamily',
+        'ts/typography/css/shorthand',
+        'ts/border/css/shorthand',
+        'ts/shadow/css/shorthand',
+        'ts/color/css/hexrgba',
+        'ts/color/modifiers',
+        'name/cti/kebab',
+        'size/px-to-rem',
+        'clamp/css',
+        'bootstrap-colors-and-scss-map',
+        'dimension/pixelToRem'
+      ],
+      options: {
+        basePxFontSize: 16
+      },
+      buildPath: 'build/scss/',
+      files: [
+        {
+          destination: '_variables.scss',
+          format: 'scss/variables',
+        },
+
+      ],
+    },
   },
+
 });
 
+sd.cleanAllPlatforms();
 sd.buildAllPlatforms();
